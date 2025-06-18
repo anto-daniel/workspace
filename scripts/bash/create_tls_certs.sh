98fe40cb5ac592a6fa5306e1b680fa15cca255b8
Anto Daniel <anto.daniel@gmail.com>
2025-06-18 15:30:38 +0000

    Updating gitignore

--- scripts/bash/create_tls_certs.sh
+++ scripts/bash/create_tls_certs.sh
@@ -9,10 +9,10 @@ DOMAIN=`hostname -f | cut -d. -f2-`
 export DOMAIN=$DOMAIN
 echo $DOMAIN
 HOSTNAME=$(hostname).$DOMAIN
-#openssl genpkey -algorithm RSA -out ca.key
+mkdir -p work 
+pushd work
 openssl genrsa -out ca.key 2048
 openssl req -new -x509 -days 3650 -key ca.key -out ca.crt -subj "/C=IN/ST=KAR/L=BLR/O=ARRM/OU=SRE/CN=$HOSTNAME"
-#openssl genpkey -algorithm RSA -out server.key
 openssl genrsa -out server.key 2048
 openssl req -new -key server.key -out server.csr -subj "/C=US/ST=CAL/L=CAL/O=CloudBolt/OU=SRE/CN=$HOSTNAME"
 cat > server.ext <<EOF
@@ -28,3 +28,5 @@ EOF
 openssl x509 -req -in server.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out server.crt -days 365 -sha256 -extfile server.ext
 cat server.key server.crt > server.pem
 openssl verify -CAfile ca.crt server.crt
+
+popd
